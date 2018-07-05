b8f57e453a2c2cab5e043be613bac340547e24d3
gbxnga <iamblizzyy@gmail.com>
2018-07-05 17:54:39 +0100

    added placeholder to input

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -93,7 +93,7 @@
 		            <form method="POST"  action="{{ route('login') }}">
                     @csrf
 		                <div class="form-group">
-		                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+		                    <input id="email" type="email" Placeholder="Email " class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 							@if ($errors->has('email'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -101,7 +101,7 @@
                                 @endif
 		                </div>
 		                <div class="form-group">
-                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                            <input id="password" type="password" Placeholder="Password "  class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
 							@if ($errors->has('password'))
                                     <span class="invalid-feedback">
