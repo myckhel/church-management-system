8284e17110e1f23cfde410da82c9dc0805951eff
myckhel <myckhel1@hotmail.com>
2018-08-30 16:47:37 +0100

    restored home controller after file lost

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Event;
+use DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = \Auth::user();
+        $members = $user->isAdmin() ? \App\Member::all() : \App\Member::where('branch_id', $user->branchcode)->get();
+        $events = $user->isAdmin() ? Event::orderBy('date', 'asc')->get() : Event::where('branch_id', $user->branchcode)->orderBy('date', 'asc')->get();
+        $options = DB::table('head_office_options')->where('HOID',1)->first();
+        $num_members = $user->isAdmin() ? DB::table('members')->count() : DB::table('members')->where('branch_id', \Auth::user()->branchcode)->count();
+        $num_pastors = $user->isAdmin() ? DB::table('members')->where('position', 'pastor')->orWhere('position', 'senior pastor')->count() : DB::table('members')->where('position', 'pastor')->orWhere('position', 'senior pastor')->where('branch_id', \Auth::user()->branchcode)->count();
+        $num_workers = $user->isAdmin() ? DB::table('members')->where('position', 'worker')->count() : DB::table('members')->where('position', 'worker')->where('branch_id', \Auth::user()->branchcode)->count();
+        $total = ['workers' => $num_workers, 'pastors' => $num_pastors, 'members' => $num_members];
+        //$events = Event::all();
+        return view('dashboard.index', compact('events','options','total','members'));
     }
 }
