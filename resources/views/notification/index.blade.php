e523e4488d43c9764e6e8acb872ae105370c6493
Hoffenheim Technologies <myckhel1@hotmail.com>
2018-09-17 18:49:12 +0100

    kenneth

--- resources/views/notification/index.blade.php
+++ resources/views/notification/index.blade.php
@@ -5,50 +5,50 @@
 
 @section('content')
 
+
 <!--CONTENT CONTAINER-->
 <!--===================================================-->
 <div id="content-container">
-    <div id="page-head">
+  <div id="page-head">
 
-        <!--Page Title-->
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-        <div id="page-title">
-            <h1 class="page-header text-overflow">General Announcements</h1>
-        </div>
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-        <!--End page title-->
+    <!--Page Title-->
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <div id="page-title">
+      <h1 class="page-header text-overflow">Create Announcements</h1>
+    </div>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <!--End page title-->
 
 
-        <!--Breadcrumb-->
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-        <ol class="breadcrumb">
-            <li>
-                <a href="forms-general.html#">
-                    <i class="demo-pli-home"></i>
-                </a>
-            </li>
-            <li>
-                <a href="{{route('dashboard')}}">Dashboard</a>
+    <!--Breadcrumb-->
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <ol class="breadcrumb">
+      <li>
+        <a href="forms-general.html#">
+          <i class="demo-pli-home"></i>
+        </a>
+      </li>
+      <li>
+         <a href="{{route('dashboard')}}">Dashboard</a>
             </li>
             <li class="active">Announcements</li>
-        </ol>
-        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-        <!--End breadcrumb-->
+    </ol>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <!--End breadcrumb-->
 
-    </div>
-
-
-    <!--Page content-->
-    <!--===================================================-->
-    <div id="page-content">
-        <div class="row">
+  </div>
 
 
-<div class="panel">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">announcements</h3>
-                                </div>
-                       <div class="col-md-9 col-lg-offset-2">
+  <!--Page content-->
+  <!--===================================================-->
+  <div id="page-content">
+    <div class="row">
+      <div class="col-lg-8 col-lg-offset-2">
+        <div class="panel">
+          <div class="panel-heading">
+            <h1 class="text-center" style="padding-top:5px">Create Announcements</h2>
+          </div>
+           <div class="col-md-9 col-lg-offset-2">
                   @if(Session::has('message'))
 <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
 @endif
@@ -61,14 +61,15 @@
 
                     @endif
                     </div>
-                                <!--No Label Form-->
-                                <!--===================================================-->
-                               <form id="send-announcement-form" role="form"  method="POST" action="{{route('calendar.announcement')}}">
+          <div class="row panel-body">
+            <div class=""  style="border:1pt solid #090c5e; border-radius:25px;">
+              <!-- BASIC FORM ELEMENTS -->
+              <!--===================================================-->
+                 <form id="send-announcement-form" role="form"  method="POST" action="{{route('calendar.announcement')}}">
                                          @csrf  <div class="panel-body">
                                         <div class="row">
-
-                                            <div class="col-md-4 mar-btm">
-                                                 <label class="col-md-2 control-label"  for="inputEmail">To</label>
+                                                <div class="col-md-4 mar-btm">
+                                                 <label class=" control-label"  for="inputEmail">To</label>
                                         <?php if(isset($_GET['mail'])) { ?>
                                                     <input type="email" id="inputEmail" name="to[]" value="<?php echo $_GET['mail']; ?> " class="form-control">
                                         <?php echo '</div>'; }else{ ?>
@@ -78,12 +79,12 @@
                                           @endforeach
                                         </select>
                                                 </div>
-
+                                      
                                   <?php }?>
-
+                                           
                                             <div class="col-md-4 mar-btm">
-                                                 <label class="col-md-2 control-label" for="inputSubject">By who</label>
-                                            <div class="col-md-9">
+                                                 <label class=" control-label" for="inputSubject">By who</label>
+                                        
                                                 <input type="text"  id="by_who" class="form-control{{ $errors->has('by_who') ? ' is-invalid' : '' }}" name="by_who" value="{{ old('by_who') }}">
                                                   <br>
                                 @if ($errors->has('by_who'))
@@ -91,10 +92,10 @@
                                         <strong>{{ $errors->first('by_who') }}</strong>
                                     </span>
                                 @endif
-                                            </div>
+                                        
                                             </div>
                                             <div class="col-md-4 mar-btm">
-                                                         <label class="col-md-2 control-label" for="inputSubject">Start Time</label>
+                                                         <label class=" control-label" for="inputSubject">Start Time</label>
                                   <div class="input-group clockpicker col-md-9">
                                 <input type="text" class="form-control" value="09:00" name="time">
                                 <span class="input-group-addon">
@@ -102,8 +103,11 @@
                                 </span>
                             </div>
                                             </div>
-                                                <div class="col-md-4 mar-btm">
-                                                         <label class="col-md-2 control-label" for="inputSubject">Stop Time</label>
+                                         
+                                         </div>   
+                                         <div class="row"> 
+                                               <div class="col-md-4 mar-btm">
+                                                         <label class=" control-label" for="inputSubject">Stop Time</label>
                                   <div class="input-group clockpicker col-md-9">
                                 <input type="text" class="form-control" value="09:30" name="stime">
                                 <span class="input-group-addon">
@@ -112,14 +116,14 @@
                             </div>
                                             </div>
                                                  <div class="col-md-4 mar-btm">
-                                                         <label class="col-md-3 control-label" for="inputSubject">Start Date</label>
+                                                         <label class="control-label" for="inputSubject">Start Date</label>
                                              <input style="border:1px solid rgba(0,0,0,0.07);height: 33px;
                                   font-size: 13px;
                                   border-radius: 3px;display: block;
                                   width: 100%;
                                    color: #555;
-                                  background-color: #fff;outline:none; margin-top:15px;padding:2px 10px" type="text" placeholder="Event Date" name="sdate" class="datepicker"/>
-
+                                  background-color: #fff;outline:none; margin-top:2px;padding:2px 10px" type="text" placeholder="Event Date" name="date" class="datepicker"/>
+                                         
                     <br>
                                 @if ($errors->has('date'))
                                     <span class="alert alert-danger">
@@ -129,14 +133,14 @@
 
                                         </div>
                                           <div class="col-md-4 mar-btm">
-                                                         <label class="col-md-3 control-label" for="inputSubject">Stop Date</label>
+                                                         <label class=" control-label" for="inputSubject">Stop Date</label>
                                              <input style="border:1px solid rgba(0,0,0,0.07);height: 33px;
                                   font-size: 13px;
                                   border-radius: 3px;display: block;
                                   width: 100%;
                                    color: #555;
-                                  background-color: #fff;outline:none; margin-top:15px;padding:2px 10px" type="text" placeholder="Event Date" name="date" class="datepicker"/>
-
+                                  background-color: #fff;outline:none; margin-top:15px;padding:2px 10px" type="text" placeholder="Event Date" name="sdate" class="datepicker"/>
+                                         
                     <br>
                                 @if ($errors->has('date'))
                                     <span class="alert alert-danger">
@@ -146,7 +150,15 @@
 
                                         </div>
 
-                                        <textarea placeholder="Message" name="message" rows="10" class="form-control"></textarea>
+                        
+                                           
+                  </div>
+                        
+
+                                           <div class="row">     
+                                         
+
+                                          <textarea placeholder="Message" name="message" rows="10" class="form-control"></textarea>
                                     </div>
                                     <div class="panel-footer text-right">
                                        <button id="mail-send-btn" type="submit" class="btn btn-primary">
@@ -154,23 +166,26 @@
                                             </button>
                                     </div>
                                 </form>
-                                <!--===================================================-->
-                                <!--End No Label Form-->
-
-                            </div>
-
-
+            </div>
+          </div>
+          <!--===================================================-->
+          <!-- END BASIC FORM ELEMENTS -->
 
 
+
+          
         </div>
-
-
+      </div>
     </div>
-    <!--===================================================-->
-    <!--End page content-->
+
+
+  </div>
+  <!--===================================================-->
+  <!--End page content-->
 
 </div>
 <!--===================================================-->
 <!--END CONTENT CONTAINER-->
 
 @endsection
+
