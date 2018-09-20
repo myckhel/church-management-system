1a396f65af41f1e7088f7d830a42a5a0cea7940f
Hoffenheim Technologies <myckhel1@hotmail.com>
2018-09-20 13:27:26 +0100

    fix

--- resources/views/mails/eventnotice.blade.php
+++ resources/views/mails/eventnotice.blade.php
@@ -1,16 +1,17 @@
 Hello,
-This is to notify you that you have been assigned to an event from <?php \Auth::user()->branchname.' '.\Auth::user()->branchcode ?>.
+This is to notify you that you have been assigned to an event from
+<?php echo ucwords(\Auth::user()->branchname.' '.\Auth::user()->branchcode); ?>.
 
-<p><b>Event Details:</b></p>
+<h2><b>Event Details:</b></h2>
 <?php
 
 ?>
-title: {{ $request->get('title') }}
-location: {{ $request->get('location') }}
-time: {{$request->get('time')}}
-assign_to: {{$request->get('title')}}
-by_who: {{$request->get('by_who')}}
-details: {{$request->get('details')}}
+<p>title: {{ $request->get('title') }}</p>
+<p>location: {{ $request->get('location') }}</p>
+<p>time: {{$request->get('time')}}</p>
+<p>assign_to: {{$request->get('title')}}</p>
+<p>by_who: {{$request->get('by_who')}}</p>
+<p>details: {{$request->get('details')}}</p>
 
-Thank You,
+<h3>Thank You</h3>
 {{ Auth::user()->branchname }}
