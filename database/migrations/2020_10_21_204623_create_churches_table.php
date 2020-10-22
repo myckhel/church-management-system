4a4d94876ff1fadfafdd89986a9e0c819be884b2
Myckhel <myckhel1@hotmail.com>
2020-10-22 01:28:01 +0100

    church/member migration

---
+++ database/migrations/2020_10_21_204623_create_churches_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChurchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('churches', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('church_id')->unsigned();
+            $table->string('name', 100);
+            $table->string('email', 50)->unique();
+            $table->string('code', 60)->unique()->nullable();
+            $table->bigInteger('country_id')->nullable()->unsigned();
+            $table->bigInteger('state_id')->nullable()->unsigned();
+            $table->string('city', 100)->nullable();
+            $table->string('address')->nullable();
+            $table->string('currency_id', 10)->nullable();
+            $table->timestamps();
+            $table->foreign('church_id')->references('id')->on('churches')->onDelete('cascade');
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
+            $table->foreign('state_id')->references('id')->on('states')->onDelete('cascade');
+            $table->foreign('currency_id')->references('currency_code')->on('countries')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('churches');
+    }
+}
---
+++ database/migrations/2020_10_21_205852_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('church_id')->unsigned();
+            $table->date('member_since')->useCurrent();
+            $table->timestamps();
+            // $table->enum('position',['worker','senior pastor','pastor', 'elder','usher','member', 'chorister','technician','instrumentalist', 'deacon','deaconess','evangelist','minister','protocol'])->default('member');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('church_id')->references('id')->on('churches')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
