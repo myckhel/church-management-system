2f13799b0f7ac2fc90b491a6d97dc3ecb7b6e33a
Myckhel <myckhel1@hotmail.com>
2020-10-24 12:27:59 +0100

    created user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,10 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Country;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class UserFactory extends Factory
 {
@@ -22,12 +24,25 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $geo = Country::select(['id', 'phonecode'])->whereHas('state')
+        ->with(['state' => fn ($q) => $q->inRandomOrder()])->inRandomOrder()->first();
+
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'email'             => $this->faker->unique()->safeEmail,
+            'title'             => $this->faker->randomElement(['Mr', 'Mrs', 'Miss','Dr (Mrs)', 'Dr', 'Prof', 'Chief', 'Chief (Mrs)', 'Engr', 'Surveyor', 'HRH','Elder']),
+            'firstname'         => $this->faker->firstName,
+            'lastname'          => $this->faker->lastName,
+            'sex'               => $this->faker->randomElement(['male', 'female']),
+            'dob'               => $this->faker->dateTimeBetween(Carbon::now()->subtract('90 years'), Carbon::now()->subtract('3 years')),
+            'phone_code'        => $geo->phonecode,
+            'phone'             => $this->faker->randomNumber(8),
+            'marital_status'    => $this->faker->randomElement(['married', 'single', 'divorced']),
+            'occupation'        => $this->faker->jobTitle,
+            'country_id'        => $geo->id,
+            'state_id'          => $geo->state->id,
+            'address'           => $this->faker->address,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password'  => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,19 +16,19 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email', 50)->unique();
-            $table->enum('title', ['Mr', 'Mrs', 'Miss','Dr (Mrs)', 'Dr', 'Prof', 'Chief', 'Chief (Mrs)', 'Engr', 'Surveyor', 'HRH','Elder','Oba','Olori'])->default('Mr');
+            $table->enum('title', ['Mr', 'Mrs', 'Miss','Dr (Mrs)', 'Dr', 'Prof', 'Chief', 'Chief (Mrs)', 'Engr', 'Surveyor', 'HRH','Elder'])->default('Mr');
             $table->string('firstname')->nullable();
             $table->string('lastname')->nullable();
+            $table->enum('sex', ['male', 'female'])->default('male');
             $table->date('dob')->nullable();
             $table->string('phone_code', 4)->nullable();
             $table->integer('phone')->nullable();
+            $table->enum('marital_status', ['married', 'single', 'divorced'])->nullable();
             $table->string('occupation', 100)->nullable();
-            $table->string('address')->nullable();
             $table->bigInteger('country_id')->nullable()->unsigned();
             $table->bigInteger('state_id')->nullable()->unsigned();
             $table->string('city', 80)->nullable();
-            $table->enum('sex', ['male', 'female'])->default('male');
-            $table->enum('marital_status', ['married', 'single'])->nullable();
+            $table->string('address')->nullable();
             $table->date('wedding_anniversary')->nullable();
             $table->string('password');
             $table->softDeletes();
