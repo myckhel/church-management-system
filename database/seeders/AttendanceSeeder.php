674059db3781769c5a3f6cc945862812ea6f25ea
Myckhel <myckhel1@hotmail.com>
2020-10-25 00:31:48 +0100

    created attendance factory

---
+++ database/seeders/AttendanceSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Attendance;
+
+class AttendanceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      Attendance::factory()->count(10)->create();
+    }
+}
