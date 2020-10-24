ed87f881b51f423e840d5eeed25a3a3b2c70bd7e
Myckhel <myckhel1@hotmail.com>
2020-10-25 00:20:49 +0100

    created event factory

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,13 +6,26 @@ use App\Traits\HasMeta;
 use App\Traits\Searchable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Service extends Model
 {
     use HasFactory, Searchable, HasMeta;
     protected $fillable = ['church_id', 'regular', 'name', 'start', 'duration', 'recurrence'];
-    protected $casts    = [];
+    protected $casts    = ['recurrence' => 'array', 'church_id' => 'int'];
     protected $searches = [];
+
+    public function makeEvent() {
+      $start = $this->start;
+      $start = isset($this->recurrence['day'])
+        ? Carbon::now()->setTime($start->hour, $start->minute, $start->second)->weekDay($this->recurrence['day'])
+        : $this->start;
+
+      $this->events()->create([
+        'start_at'  => $start,
+        'end_at'    => Carbon::parse($start)->add('mins', $this->duration),
+      ]);
+    }
 
     public function church(){
       return $this->belongsTo(Church::class);
