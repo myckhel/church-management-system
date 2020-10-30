76132f31d42ec3a6d3c6cdf7d921ae9de0ca54f8
Myckhel <myckhel1@hotmail.com>
2020-10-30 23:50:25 +0100

    added request church macro

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use App\Traits\HasMeta;
 use App\Traits\Searchable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Traits\HasRoles;
 use Spatie\Permission\Traits\HasPermissions;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Member extends Model
+class Member extends Authenticatable
 {
     use HasPermissions, HasApiTokens, HasRoles, HasFactory, Searchable, HasMeta;
     protected $fillable = ['user_id', 'church_id', 'member_since'];
