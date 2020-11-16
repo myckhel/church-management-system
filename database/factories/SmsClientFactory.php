616a33d79467dcdc15d3a2909bc1f19df470666a
Myckhel <myckhel1@hotmail.com>
2020-11-16 23:28:32 +0100

    created sms client seeder

---
+++ database/factories/SmsClientFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SmsClient;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SmsClientFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = SmsClient::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+          'name' => $this->faker->word.' Sms',
+          'endpoint' => $this->faker->url,
+          'auth_type' => $this->faker->randomElement(['basic auth', 'api key', 'bearer token', 'oath 2.0']),
+          'auth' => json_encode($this->faker->randomElement([
+            ['username' => 'name', 'password' => 'password'],
+            ['key' => 'key', 'value' => 'value', 'at' => $this->faker->randomElement(['header', 'query'])],
+            ['token' => 'token'],
+            ['token' => 'token', 'name' => 'name']
+          ])),
+        ];
+    }
+}
