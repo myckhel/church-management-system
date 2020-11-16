1382308cbfc7821797caf99be600adcc922f2df9
Myckhel <myckhel1@hotmail.com>
2020-11-16 23:28:32 +0100

    created sms client seeder

---
+++ database/seeders/SmsClientSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\SmsClient;
+use App\Models\Church;
+use Faker\Factory as Faker;
+
+class SmsClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $faker = Faker::create();
+      $cid = Church::inRandomOrder()->limit(300)->pluck('id');
+      SmsClient::factory()->count($faker->numberBetween(50, 300))
+      ->state(['church_id'=> fn () => $faker->randomElement($cid)])
+      ->create();
+    }
+}
