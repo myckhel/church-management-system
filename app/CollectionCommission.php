654c7c88f3b8f02e70b9d9a7200cc30c06222ff7
Myckhel <myckhel1@hotmail.com>
2023-04-30 17:33:00 +0100

    fixed null percentage bug

--- app/CollectionCommission.php
+++ app/CollectionCommission.php
@@ -14,120 +14,135 @@ class CollectionCommission extends Model
     ];
     protected $table = 'collections_commissions';
 
-    public static function getUserUnsettled(){
-      return auth()->user()->collections_commissions->where('settled', false);
+    public static function getUserUnsettled()
+    {
+        return auth()->user()->collections_commissions->where('settled', false);
     }
 
-    public static function savingsPercentage(array $savings){
-      $percentage = (float)(\App\Options::getLatestCommission())->value;
-      $total = 0.00;
-      foreach ($savings as $key => $saving) {
-        $total += (float)$saving->total;
-      }
-      return number_format((float)($total * ($percentage / 100)), 2, '.', '');
+    public static function savingsPercentage(array $savings)
+    {
+        $percentage = (float)(\App\Options::getLatestCommission())?->value;
+        $total = 0.00;
+        foreach ($savings as $key => $saving) {
+            $total += (float)$saving->total;
+        }
+        return number_format((float)($total * ($percentage / 100)), 2, '.', '');
     }
 
-    public static function mySelfByDate($date){
-      return auth()->user()->collections_commissions->where('collection_date', $date)->where('settled', false)->first();
+    public static function mySelfByDate($date)
+    {
+        return auth()->user()->collections_commissions->where('collection_date', $date)->where('settled', false)->first();
     }
 
-    public static function getDueCommissions(User $user = null){
-      return $due = CollectionCommission::
-        select('users.id as branch_id', 'collections.id', 'collections.service_types_id', 'collections.collections_types_id', 'collections.amount',
-         'collections_commissions.collection_date')
-        ->where(isset($user) ? 'collections_commissions.branch_id' : [] , isset($user) ? $user->id : [])
-        ->where('settled', false)
-        // ->with('savings.date_collected')
-        // ->with('users')
-        ->leftJoin('collections', 'collections.date', 'collection_date')
-        ->leftJoin('users', 'users.id', 'collections.branch_id')
-        ->where(isset($user) ? 'collections.branch_id' : [], isset($user) ? $user->id : [])
-        ->get();
+    public static function getDueCommissions(User $user = null)
+    {
+        return $due = CollectionCommission::select(
+                'users.id as branch_id',
+                'collections.id',
+                'collections.service_types_id',
+                'collections.collections_types_id',
+                'collections.amount',
+                'collections_commissions.collection_date'
+            )
+            ->where(isset($user) ? 'collections_commissions.branch_id' : [], isset($user) ? $user->id : [])
+            ->where('settled', false)
+            // ->with('savings.date_collected')
+            // ->with('users')
+            ->leftJoin('collections', 'collections.date', 'collection_date')
+            ->leftJoin('users', 'users.id', 'collections.branch_id')
+            ->where(isset($user) ? 'collections.branch_id' : [], isset($user) ? $user->id : [])
+            ->get();
     }
 
-    public static function dueSavings(User $user = null){
-      $dueRows = CollectionCommission::getDueCommissions(isset($user) ? $user: null);
-      // dd($dueRows);
-      $savings = [];
-      foreach ($dueRows as $key => $commission) {
-        //
-        if (!isset($savings[$commission->branch_id])) {
-          $savings[$commission->branch_id] = [];
+    public static function dueSavings(User $user = null)
+    {
+        $dueRows = CollectionCommission::getDueCommissions(isset($user) ? $user : null);
+        // dd($dueRows);
+        $savings = [];
+        foreach ($dueRows as $key => $commission) {
+            //
+            if (!isset($savings[$commission->branch_id])) {
+                $savings[$commission->branch_id] = [];
+            }
+            //
+            $savings[$commission->branch_id][] = \App\Collection::find($commission->id);
         }
         //
-        $savings[$commission->branch_id][] = \App\Collection::find($commission->id);
-      }
-      //
-      foreach ($savings as $key => $value) {
-        // code...
-        $savings[$key] = \App\Collection::rowToColumn($value, 'no_obj');
-      }
-      // dd($savings);
-      return $savings;
-    }
-
-    public static function calculateUnsettledCommission(bool $type = false){
-      $user = \Auth::user();
-      $percentage = \App\Options::getLatestCommission();
-      $percentage = $percentage ? (float)$percentage->value : 0;
-      $total = $type ? [] : 0;
-      $dueCommissions = $type ? [] : 0;
-      if ($type) {
-        // get the
-        $unsettle = CollectionCommission::dueSavings();
-        // declare a total class to contain savings
-        foreach ($unsettle as $branch_id => $branch) {
-          // dd($saving);
-          if (!isset($total[$branch_id])) {
-            $total[$branch_id] = 0;
-          }
-          foreach ($branch as $date => $saving) {
-            // dd($saving);
-            $total[$branch_id] += (float)$saving->total;
-          }
+        foreach ($savings as $key => $value) {
+            // code...
+            $savings[$key] = \App\Collection::rowToColumn($value, 'no_obj');
         }
-        // calculate each branch commission
-        foreach ($total as $key => $value) {
-          // code...
-          $dueCommissions[$key] = (float)($value * ($percentage / 100));
+        // dd($savings);
+        return $savings;
+    }
+
+    public static function calculateUnsettledCommission(bool $type = false)
+    {
+        $user = \Auth::user();
+        $percentage = \App\Options::getLatestCommission();
+        $percentage = $percentage ? (float)$percentage->value : 0;
+        $total = $type ? [] : 0;
+        $dueCommissions = $type ? [] : 0;
+        if ($type) {
+            // get the
+            $unsettle = CollectionCommission::dueSavings();
+            // declare a total class to contain savings
+            foreach ($unsettle as $branch_id => $branch) {
+                // dd($saving);
+                if (!isset($total[$branch_id])) {
+                    $total[$branch_id] = 0;
+                }
+                foreach ($branch as $date => $saving) {
+                    // dd($saving);
+                    $total[$branch_id] += (float)$saving->total;
+                }
+            }
+            // calculate each branch commission
+            foreach ($total as $key => $value) {
+                // code...
+                $dueCommissions[$key] = (float)($value * ($percentage / 100));
+            }
+        } else {
+            $unsettle = CollectionCommission::getDueCommissions($user);
+            // dd($unsettle);
+            foreach ($unsettle as $key => $saving) {
+                // dd($saving);
+                $total += (int)$saving->amount;
+            }
+            // calculate the percentage
+            $dueCommissions = number_format((float)($total * ($percentage / 100)), 2, '.', '');
         }
-      } else {
-        $unsettle = CollectionCommission::getDueCommissions($user);
-        // dd($unsettle);
-        foreach ($unsettle as $key => $saving) {
-          // dd($saving);
-          $total += (int)$saving->amount;
+        return $dueCommissions;
+    }
+
+    public static function setCollection(Collection $savings)
+    {
+        $user = \Auth::user();
+        return CollectionCommission::create([
+            'collection_date' => $savings->date,
+            'branch_id' => $user->id,
+        ]);
+    }
+
+    public static function undue(array $ids)
+    {
+        foreach ($ids as $key => $due) {
+            // code...
+            $due = self::find($due);
+            // dd($due);
+            $due->settled = true;
+            $due->save();
         }
-        // calculate the percentage
-        $dueCommissions = number_format((float)($total * ($percentage / 100)), 2, '.', '');
-      }
-      return $dueCommissions;
+        return true;
     }
 
-    public static function setCollection(Collection $savings){
-      $user = \Auth::user();
-      return CollectionCommission::create([
-        'collection_date' => $savings->date,
-        'branch_id' => $user->id,
-      ]);
+    public function users()
+    {
+        return $this->belongsTo(User::class);
     }
 
-    public static function undue(array $ids){
-      foreach ($ids as $key => $due) {
-        // code...
-        $due = self::find($due);
-        // dd($due);
-        $due->settled = true;
-        $due->save();
-      }
-      return true;
-    }
-
-    public function users(){
-      return $this->belongsTo(User::class);
-    }
-
-    public function savings(){
-      return $this->belongsTo(Collection::class, 'date');
+    public function savings()
+    {
+        return $this->belongsTo(Collection::class, 'date');
     }
 }
--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -20,7 +20,7 @@ class BranchController extends Controller
 
     public function __construct()
     {
-      $this->user = \Auth::user();
+        $this->user = \Auth::user();
     }
     /**
      * Display a listing of the resource.
@@ -29,20 +29,21 @@ class BranchController extends Controller
      */
     public function index(Request $request)
     {
-      $user = \Auth::user();
-      if(!$user->isAdmin()){
-        return redirect()->route('dashboard');
-      }
-      //$members = Member::all();
-      if ($request->draw) {
-        return DataTables::of(User::all())->make(true);
-      } else {
-        return view('branch.all');
-      }
+        $user = \Auth::user();
+        if (!$user->isAdmin()) {
+            return redirect()->route('dashboard');
+        }
+        //$members = Member::all();
+        if ($request->draw) {
+            return DataTables::of(User::all())->make(true);
+        } else {
+            return view('branch.all');
+        }
     }
 
-    public function users(){
-      return DataTables::of(User::all())->make(true);
+    public function users()
+    {
+        return DataTables::of(User::all())->make(true);
     }
 
     /**
@@ -57,7 +58,7 @@ class BranchController extends Controller
         $id = $request->id;
         $user = \Auth::user();
         DB::table('users')->where('id', '=', $id)->delete();
-         return response()->json(['success' => true,]);
+        return response()->json(['success' => true,]);
     }
 
     public function registerForm()
@@ -73,29 +74,29 @@ class BranchController extends Controller
 
     public function register(Request $request)
     {
-      $data = [];
-      $data['branchname'] = $request->branchname;
-      $data['branchcode'] = $request->branchcode;
-      $data['address'] = $request->address;
-      $data['email'] = $request->email;
-      $data['country'] = $request->country;
-      $data['state'] = $request->state;
-      $data['city'] = $request->city;
-      if (!User::first()) {
-        $data['isadmin'] = true;
-      }
-      $data['currency'] = $request->currency;
-      $data['password'] = $request->password;
-      $data['password_confirmation'] = $request->password_confirmation;
+        $data = [];
+        $data['branchname'] = $request->branchname;
+        $data['branchcode'] = $request->branchcode;
+        $data['address'] = $request->address;
+        $data['email'] = $request->email;
+        $data['country'] = $request->country;
+        $data['state'] = $request->state;
+        $data['city'] = $request->city;
+        if (!User::first()) {
+            $data['isadmin'] = true;
+        }
+        $data['currency'] = $request->currency;
+        $data['password'] = $request->password;
+        $data['password_confirmation'] = $request->password_confirmation;
 
-      $validate = self::validator($data);
-      if($validate->fails()){
-        return redirect()->back()->withErrors($validate)->withInput();
-      }
-      $creation = self::creator($data);
-      //
-      $success = 'Successfully Registered';
-      return redirect()->back()->with('status', $success);
+        $validate = self::validator($data);
+        if ($validate->fails()) {
+            return redirect()->back()->withErrors($validate)->withInput();
+        }
+        $creation = self::creator($data);
+        //
+        $success = 'Successfully Registered';
+        return redirect()->back()->with('status', $success);
     }
 
     protected function validator(array $data)
@@ -115,93 +116,99 @@ class BranchController extends Controller
 
     protected function creator(array $data)
     {
-      $branch = User::create([
-        'branchname' => $data['branchname'],
-        'branchcode' => $data['branchcode'],
-        'address' => $data['address'],
-        'email' => $data['email'],
-        'isadmin' => isset($data['isadmin']) ? $data['isadmin'] : 'false',
-        'password' => Hash::make($data['password']),
-        'country' => $data['country'],
-        'state' => $data['state'],
-        'city' => $data['city'],
-        'currency' => $data['currency'],
-      ]);
+        $branch = User::create([
+            'branchname' => $data['branchname'],
+            'branchcode' => $data['branchcode'],
+            'address' => $data['address'],
+            'email' => $data['email'],
+            'isadmin' => isset($data['isadmin']) ? $data['isadmin'] : 'false',
+            'password' => Hash::make($data['password']),
+            'country' => $data['country'],
+            'state' => $data['state'],
+            'city' => $data['city'],
+            'currency' => $data['currency'],
+        ]);
 
-      if (!$branch) {
-        return $branch;
-      }
-
+        if (!$branch) {
+            return $branch;
+        }
     }
 
-    public function ho(Request $request){
-      $user = \Auth::user();
-      $options = \App\head_office_options::all();
+    public function ho(Request $request)
+    {
+        $user = \Auth::user();
+        $options = \App\head_office_options::all();
 
-      return view('branch.ho', ['options' => $options]);
+        return view('branch.ho', ['options' => $options]);
     }
 
-    public function delete(Request $request){
-      $failed = 0;
-      $text = "All selected branches were deleted successfully";
-      foreach ($request->id as $key => $value) {
-        $branch = User::whereId($value)->first();
-        if($branch){
-          $branch->delete();
+    public function delete(Request $request)
+    {
+        $failed = 0;
+        $text = "All selected branches were deleted successfully";
+        foreach ($request->id as $key => $value) {
+            $branch = User::whereId($value)->first();
+            if ($branch) {
+                $branch->delete();
+            } else {
+                $failed++;
+                $text = "$failed Operations could not be performed";
+            }
+        }
+        return response()->json(['status' => true, 'text' => $text]);
+    }
+
+    public function tools()
+    {
+        return view('branch.tools');
+    }
+
+    public function options()
+    {
+        return view('branch.options');
+    }
+
+    public function updateBranch(Request $request)
+    {
+        $branch = User::whereId($request->id)->first();
+        // dd($request);
+        if ($branch) {
+            $errors = [];
+            $fields = (array)$request->request; //->parameters;//->ParameterBag->parameters;
+            $fields = $fields["\x00*\x00parameters"];
+            foreach ($fields as $key => $value) {
+                if ($key != 'id' && $key != '_token' && $key != 'action') {
+                    $branch->$key = $request->$key;
+                }
+            }
+            try {
+                $branch->save();
+            } catch (\Exception $e) {
+                array_push($errors, $e);
+                // dd($e);
+                return response()->json(['status' => false, 'text' => $e->errorInfo[2]]);
+            }
         } else {
-          $failed++;
-          $text = "$failed Operations could not be performed";
+            return response()->json(['status' => false, 'text' => "Branch does not exist"]);
         }
-      }
-      return response()->json(['status' => true, 'text' => $text]);
+        return response()->json(['status' => true, 'text' => "Branch has been updated!"]);
     }
 
-    public function tools(){
-      return view('branch.tools');
-    }
-
-    public function options(){
-      return view('branch.options');
-    }
-
-    public function updateBranch(Request $request){
-      $branch = User::whereId($request->id)->first();
-      // dd($request);
-      if($branch) {
-        $errors = [];
-        $fields = (array)$request->request;//->parameters;//->ParameterBag->parameters;
-        $fields = $fields["\x00*\x00parameters"];
-        foreach ($fields as $key => $value) {
-          if ($key != 'id' && $key != '_token' && $key != 'action') {
-              $branch->$key = $request->$key;
-          }
-        }
-        try {
-          $branch->save();
-        } catch (\Exception $e) {
-          array_push($errors, $e);
-          // dd($e);
-          return response()->json(['status' => false, 'text' => $e->errorInfo[2]]);
-        }
-      }
-      else {return response()->json(['status' => false, 'text' => "Branch does not exist"]);}
-      return response()->json(['status' => true, 'text' => "Branch has been updated!"]);
-    }
-
-    public function invoice(Request $request){
-      $user = \Auth::user();
-      // get due savings
-      $dueSavings = \App\CollectionCommission::dueSavings($user);
-      // if nothing found for the branch
-      // redirect back withErrors
-      if(!isset($dueSavings[$user->id])) return back()->withErrors(['message' => 'Nothing to pay']);
-      // get the commission percentage
-      $percentage = (int)(\App\Options::getLatestCommission())->value;
-      // dd($dueSavings);
-      $details = \App\Options::getLatestCommissionBankDetails();
-      // app logo
-      $logo = \App\Setting::findName(['logo']);
-      $blanceDue = \App\CollectionCommission::calculateUnsettledCommission();
-      return view('branch.invoice', compact('details', 'dueSavings', 'percentage', 'blanceDue', 'user', 'logo'));
+    public function invoice(Request $request)
+    {
+        $user = \Auth::user();
+        // get due savings
+        $dueSavings = \App\CollectionCommission::dueSavings($user);
+        // if nothing found for the branch
+        // redirect back withErrors
+        if (!isset($dueSavings[$user->id])) return back()->withErrors(['message' => 'Nothing to pay']);
+        // get the commission percentage
+        $percentage = (int)(\App\Options::getLatestCommission())?->value;
+        // dd($dueSavings);
+        $details = \App\Options::getLatestCommissionBankDetails();
+        // app logo
+        $logo = \App\Setting::findName(['logo']);
+        $blanceDue = \App\CollectionCommission::calculateUnsettledCommission();
+        return view('branch.invoice', compact('details', 'dueSavings', 'percentage', 'blanceDue', 'user', 'logo'));
     }
 }
