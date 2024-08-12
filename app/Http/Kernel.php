b93037491ce76a51354d57cf2d83d5f0d89a4d48
Myckhel <myckhel123@gmail.com>
2024-08-13 00:42:27 +0100

    applied isadmin role to routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,5 +61,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
     ];
 }
